4d506ef7cbc7385cf91c5578c0cd80142e7b2bfb
alheym <alheym.mw@gmail.com>
2023-01-08 20:49:51 +0300

    fix

--- src/modules/chat/chat.js
+++ src/modules/chat/chat.js
@@ -100,16 +100,17 @@ export default function Chat() {
     const profile = [
         {
             id: '',
-            profileName: 'Иван',
+            first_name: 'Иван',
             email: 'pochta@yandex.ru',
             login: 'ivanivanov',
             phone: '+7(909) 967 30 30',
             second_name: 'Иванов',
+            display_name: 'Иван',
         }
     ];
 
     const profileList = profile.map((item) => {
-        return Profile(item.id, item.profileName, item.email, item.login, item.phone, item.second_name);
+        return Profile(item.id, item.first_name, item.email, item.login, item.phone, item.second_name, item.display_name);
     }).join('');
 
     const editEmail = [
