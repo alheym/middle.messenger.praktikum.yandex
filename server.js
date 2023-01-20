9573c7f1ba40e8008b656df31f3690a082a2d507
alheym <alheym.mw@gmail.com>
2023-01-20 11:15:00 +0300

    fix

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
-const path = require('path');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('./dist'));
-app.use('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+app.use(express.static(`${__dirname}/dist/`));
+
+app.get('*', (req, res) => {
+	res.sendFile(`${__dirname}/dist/`);
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+app.listen(PORT, function () {
+	console.log(`Example app listening on port ${PORT}!`);
 });
