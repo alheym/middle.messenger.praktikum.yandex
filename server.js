e569f5b53d666d2c92b7317396f62339f181f1a7
alheym <alheym.mw@gmail.com>
2023-01-20 10:59:11 +0300

    fix

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const PORT = process.env.PORT || 3000;
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+// const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname +'./dist'));
-app.use('/*', (req, res) => {
-	res.sendFile(path.join(__dirname, 'dist/index.html'));
+// app.use(express.static(__dirname +'./dist'));
+// app.use('/*', (req, res) => {
+// 	res.sendFile(path.join(__dirname, 'dist/index.html'));
+// });
+
+// app.listen(PORT, () => {
+// 	console.log(`Example app listening on port ${PORT}!`);
+// });
+
+// noinspection JSCheckFunctionSignatures
+
+const express = require('express');
+// const path = require('path');
+
+const app = express();
+// const PORT = process.env.PORT || 3000;
+const PORT = 3000;
+
+app.use(express.static(__dirname + '/dist'));
+
+app.use("/*", (req, res) => {
+	// res.sendFile(path.join(__dirname, 'dist/index.html'));
+	res.sendFile(`${__dirname}/dist/index.html`);
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, function () {
 	console.log(`Example app listening on port ${PORT}!`);
 });
