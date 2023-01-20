202679931a2d2ff77be545f125fd47cae3e70ff2
alheym <alheym.mw@gmail.com>
2023-01-20 12:01:10 +0300

    fix

--- server.js
+++ server.js
@@ -1,32 +1,12 @@
-// const express = require('express');
-
-// const app = express();
-// const PORT = process.env.PORT || 3000;
-
-// app.use(express.static(`${__dirname}/dist/`));
-
-// app.get('*', (req, res) => {
-// 	res.sendFile(`${__dirname}/dist/`);
-// });
-
-// app.listen(PORT, function () {
-// 	console.log(`Example app listening on port ${PORT}!`);
-// });
-
-// noinspection JSCheckFunctionSignatures
-
 const express = require('express');
-// const path = require('path');
 
 const app = express();
-// const PORT = process.env.PORT || 3000;
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(`${__dirname}/dist/`));
 
-app.use("/*", (req, res) => {
-	// res.sendFile(path.join(__dirname, 'dist/index.html'));
-	res.sendFile(`${__dirname}/dist/index.html`);
+app.get('*', (req, res) => {
+	res.sendFile(`${__dirname}/dist/`);
 });
 
 app.listen(PORT, function () {
