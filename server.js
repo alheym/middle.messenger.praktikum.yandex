63aed4df53436dad2c4e9fffa7180396d38220bb
alheym <alheym.mw@gmail.com>
2023-01-20 09:44:40 +0300

    created HTTPTransport

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
-const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname + '/dist')));
+app.use(express.static(`${__dirname}/dist/`));
+
+app.get('*', (req, res) => {
+	res.sendFile(`${__dirname}/dist/`);
+});
 
 app.listen(PORT, function () {
-  console.log(`Example app listening on port ${PORT}!`);
-}); 
+	console.log(`Example app listening on port ${PORT}!`);
+});
