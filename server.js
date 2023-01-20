c8fa7bd695a1030256ed87d4d6d2299c78766553
alheym <alheym.mw@gmail.com>
2023-01-20 10:57:46 +0300

    server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('./dist'));
+app.use(express.static(__dirname +'./dist'));
 app.use('/*', (req, res) => {
 	res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
