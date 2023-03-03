4c7afffc6d02a20f1d87bcef08caeece9f63425e
alheym <alheym.mw@gmail.com>
2023-03-03 12:29:58 +0300

    dist

---
+++ dist/index.44983732.js
@@ -0,0 +1,7 @@
+function onChangeAvatar() {
+    var imageName = document.getElementById("imageName");
+    var inputFile = document.getElementById("avatar");
+    imageName.innerText = inputFile.files[0].name;
+}
+
+//# sourceMappingURL=index.44983732.js.map
